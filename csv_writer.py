9b99d60537ec0da10de557d1d0c3b0d8d85b7dc7
Dany <danymike.joseph@gmail.com>
2022-04-02 22:17:22 +0200

    remove useless files

--- csv_writer.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-#Read first element of the csv
-with open('/home/dany/data_analysis/players_stats.csv', 'r') as players_stats:
-    reader = csv.reader(players_stats, delimiter=',')
-    for row in reader:
-        print(row[0])
--- script.py
+++
@@ -1,3 +0,0 @@
-positions = ['RM, CAM, LM, CM', 'GK', 'CAM, ST, CF', 'CDM, CM', 'LB, RB, CDM', 'RB', 'CB', 'LM, RM', 'ST', 'RM, LM', 'CM, CAM, CDM', 'CM, CDM', 'CAM, LW', 'RB, CB', 'LM', 'RM', 'CB, LB', 'CAM, RW, LW']
-final_positions = list( dict.fromkeys(positions) )
-print(final_positions)
